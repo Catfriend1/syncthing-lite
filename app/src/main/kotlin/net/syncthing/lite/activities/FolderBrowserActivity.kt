20ccab97b29447be0bff97b127f4651732dc771d
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-06-30 05:43:30 +0200

    import androidx.databinding.DataBindingUtil

--- app/src/main/kotlin/net/syncthing/lite/activities/FolderBrowserActivity.kt
+++ app/src/main/kotlin/net/syncthing/lite/activities/FolderBrowserActivity.kt
@@ -2,7 +2,7 @@ package net.syncthing.lite.activities
 
 import android.app.Activity
 import android.content.Intent
-import android.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
