b157eec6bf175654fb338cfbb3bf72cf0b36d6b9
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-08 00:21:34 +0200

    Update AddressRanker.kt

--- syncthing-discovery/src/main/kotlin/net/syncthing/java/discovery/utils/AddressRanker.kt
+++ syncthing-discovery/src/main/kotlin/net/syncthing/java/discovery/utils/AddressRanker.kt
@@ -28,8 +28,8 @@ object AddressRanker {
 
     private const val TCP_CONNECTION_TIMEOUT = 5000
     private val BASE_SCORE_MAP = mapOf(
-            AddressType.TCP to 0,
-            AddressType.RELAY to 2000
+        AddressType.TCP to 0,
+        AddressType.RELAY to 2000
     )
     private val ACCEPTED_ADDRESS_TYPES = BASE_SCORE_MAP.keys
     private val logger = LoggerFactory.getLogger(AddressRanker::class.java)
@@ -46,7 +46,7 @@ object AddressRanker {
                                 // this nested async ensures that cancelling/ the timeout has got an effect without delay
                                 withContext(Dispatchers.IO) {
                                     pingAddressSync(address)
-                                }.await()
+                                }
                             }
 
                             if (addressWithScore != null) {
@@ -61,14 +61,15 @@ object AddressRanker {
                 }
                 .map { it.await() }
 
-        close()
-    }
+            close()
+        }
 
     @Deprecated(
-            message = "This is slower than the version which returns the channel",
-            replaceWith = ReplaceWith("pingAddressesChannel")
+        message = "This is slower than the version which returns the channel",
+        replaceWith = ReplaceWith("pingAddressesChannel")
     )
-    suspend fun pingAddressesReturnAllResultsAtOnce(sourceAddresses: List<DeviceAddress>) = pingAddressesChannel(sourceAddresses)
+    suspend fun pingAddressesReturnAllResultsAtOnce(sourceAddresses: List<DeviceAddress>) =
+        pingAddressesChannel(sourceAddresses)
             .toList()
             .sortedBy { it.score }
 
