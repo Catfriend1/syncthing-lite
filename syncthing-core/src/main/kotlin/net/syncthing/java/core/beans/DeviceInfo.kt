8c2155a9a11023a336b0723bfccca57f431fbf3d
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-07 12:36:24 +0200

    Revert "DeviceInfo parcelable"
    This reverts commit ca4217b7d7934aff75ec779a5971ad6e2ebcd622.

--- syncthing-core/src/main/kotlin/net/syncthing/java/core/beans/DeviceInfo.kt
+++ syncthing-core/src/main/kotlin/net/syncthing/java/core/beans/DeviceInfo.kt
@@ -1,15 +1,24 @@
+/*
+ * Copyright 2016 Davide Imbriaco <davide.imbriaco@gmail.com>
+ * Copyright 2018 Jonas Lochmann
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.syncthing.java.core.beans
 
-import android.os.Parcelable
 import com.google.gson.stream.JsonReader
 import com.google.gson.stream.JsonWriter
-import kotlinx.parcelize.Parcelize
+import java.io.Serializable
 
-@Parcelize
-data class DeviceInfo(
-    val deviceId: DeviceId,
-    val name: String
-) : Parcelable {
+data class DeviceInfo(val deviceId: DeviceId, val name: String): Serializable {
 
     companion object {
         private const val DEVICE_ID = "deviceId"
@@ -30,17 +39,20 @@ data class DeviceInfo(
             reader.endObject()
 
             return DeviceInfo(
-                deviceId = deviceId!!,
-                name = name!!
+                    deviceId = deviceId!!,
+                    name = name!!
             )
         }
     }
 
     fun serialize(writer: JsonWriter) {
         writer.beginObject()
+
         writer.name(DEVICE_ID)
         deviceId.serialize(writer)
+
         writer.name(NAME).value(name)
+
         writer.endObject()
     }
 }
