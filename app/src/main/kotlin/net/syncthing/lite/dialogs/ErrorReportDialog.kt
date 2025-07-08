e1ab5dd431118f186229d20a565bd9b2aed19500
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-08 15:58:39 +0200

    Update ErrorReportDialog.kt

--- app/src/main/kotlin/net/syncthing/lite/dialogs/ErrorReportDialog.kt
+++ app/src/main/kotlin/net/syncthing/lite/dialogs/ErrorReportDialog.kt
@@ -24,7 +24,7 @@ class ErrorReportDialog : DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val report = requireArguments().getString(REPORT)
+        val report = arguments!!.getString(REPORT)
         val ctx = requireContext()
         val clipboard = ctx.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
 
