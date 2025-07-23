a34fd1172418c099ba8e7a7db334d9fd31132207
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-23 19:51:54 +0200

    Update SyncthingActivity.kt

--- app/src/main/kotlin/net/syncthing/lite/activities/SyncthingActivity.kt
+++ app/src/main/kotlin/net/syncthing/lite/activities/SyncthingActivity.kt
@@ -6,12 +6,10 @@ import android.os.Bundle
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import android.view.LayoutInflater
-import kotlinx.coroutines.launch
 import net.syncthing.lite.R
 import net.syncthing.lite.async.CoroutineActivity
 import net.syncthing.lite.databinding.DialogLoadingBinding
 import net.syncthing.lite.library.LibraryHandler
-import net.syncthing.lite.library.ConnectionManager
 
 abstract class SyncthingActivity : CoroutineActivity() {
     val libraryHandler: LibraryHandler by lazy {
@@ -21,8 +19,6 @@ abstract class SyncthingActivity : CoroutineActivity() {
     }
     private var loadingDialog: AlertDialog? = null
     private var snackBar: Snackbar? = null
-    protected lateinit var connectionManager: ConnectionManager
-    private var isStarted = false
 
     companion object {
         private const val TAG = "SyncthingActivity"
@@ -30,12 +26,10 @@ abstract class SyncthingActivity : CoroutineActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        connectionManager = ConnectionManager(libraryHandler, this, TAG)
     }
 
     override fun onStart() {
         super.onStart()
-        isStarted = true
 
         val binding = DataBindingUtil.inflate<DialogLoadingBinding>(
                 LayoutInflater.from(this), R.layout.dialog_loading, null, false)
@@ -56,33 +50,12 @@ abstract class SyncthingActivity : CoroutineActivity() {
 
     override fun onStop() {
         super.onStop()
-        isStarted = false
 
-        connectionManager.stop()
         libraryHandler.stop()
         loadingDialog?.dismiss()
     }
 
     open fun onLibraryLoaded() {
-        // For MainActivity, enable discovery when library is loaded
-        if (this is MainActivity) {
-            libraryHandler.enableLocalDiscovery()
-            libraryHandler.enableGlobalDiscovery()
-        }
-        
-        // Start connection manager (no conditions for SyncthingActivity)
-        connectionManager.start()
-        
-        // For MainActivity, ensure immediate connection attempt
-        if (this is MainActivity) {
-            connectionManager.triggerImmediateConnectionAttempt()
-        }
-    }
-
-    /**
-     * Trigger immediate discovery and connection (called when new devices are added)
-     */
-    fun triggerImmediateConnectionAttempt() {
-        connectionManager.triggerImmediateConnectionAttempt()
+        // nothing to do
     }
 }
