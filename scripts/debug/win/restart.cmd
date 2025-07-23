550fa0583e6bf38f3b1d3119d92742431fc9cd6b
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-23 13:14:04 +0200

    Update restart.cmd

--- scripts/debug/win/restart.cmd
+++ scripts/debug/win/restart.cmd
@@ -5,7 +5,7 @@ SET "PACKAGE_NAME=com.github.catfriend1.syncthinglite.debug"
 ::
 call stop.cmd
 ::
-:: adb shell rm -rf /data/data/%PACKAGE_NAME%/cache/*
+:: adb shell rm -rf /data/data/%PACKAGE_NAME%/cache/
 ::
 adb shell am start -n "%PACKAGE_NAME%/net.syncthing.lite.activities.MainActivity"
 ::
