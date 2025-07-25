edf8495b36f3604b4482b870e52f47015b75ea1b
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-25 02:05:43 +0200

    Update build.gradle.kts

--- syncthing-client/build.gradle.kts
+++ syncthing-client/build.gradle.kts
@@ -1,11 +1,21 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+android {
+    compileSdk = libs.versions.compile.sdk.get().toInt()
+    namespace = "net.syncthing.java.client"
+
+    defaultConfig {
+        minSdk = libs.versions.min.sdk.get().toInt()
+        targetSdk = libs.versions.target.sdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
 }
 
 dependencies {
