a7b3d92d7e2cc792413c11f04dde94a085165a8d
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-27 14:32:48 +0200

    Update build.gradle.kts

--- syncthing-core/build.gradle.kts
+++ syncthing-core/build.gradle.kts
@@ -15,4 +15,9 @@ dependencies {
     api(libs.bouncy.castle)
     implementation(libs.kotlin.stdlib)
     implementation(libs.kotlinx.coroutines.core)
+    
+    implementation("org.bouncycastle:bctls-jdk15to18:1.81")
+    implementation("org.bouncycastle:bcpkix-jdk15to18:1.81")
+    implementation("org.bouncycastle:bcutil-jdk15to18:1.81")
+    implementation("org.bouncycastle:bcprov-jdk15to18:1.81")
 }
