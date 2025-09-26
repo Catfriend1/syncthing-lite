032e5a5c2a88d2adcd9386ed84597ea7dec5b881
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-09-26 10:55:58 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
         maven { url = uri("https://jitpack.io") }
     }
 }
